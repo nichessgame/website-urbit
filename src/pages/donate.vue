9488f93e1a626725d731f485a3ed4df3f746dee4
vasilije2448 <73365189+vasilije2448@users.noreply.github.com>
2025-12-28 12:00:58 +0100

    match style

--- src/pages/donate.vue
+++ src/pages/donate.vue
@@ -1,13 +1,16 @@
+
 <template>
   <v-container class="mt-6" max-width="900">
-    <div class="text-h4"> Donate </div>
-    <div class="mt-4">
-    If you'd like to see a multiplayer server, consider donating.
-    </div>
-    <div class="mt-2">
-      Patreon: <a class="custom-link"
-        href="https://www.patreon.com/join/nichessgame">https://www.patreon.com/join/nichessgame</a>
-    </div>
+    <v-card class="pa-6" elevation="4">
+      <div class="text-h4"> Donate </div>
+      <div class="mt-4">
+      If you'd like to see a multiplayer server, consider donating.
+      </div>
+      <div class="mt-2">
+        Patreon: <a class="custom-link"
+          href="https://www.patreon.com/join/nichessgame">https://www.patreon.com/join/nichessgame</a>
+      </div>
+    </v-card>
   </v-container>
 </template>
 
@@ -22,5 +25,11 @@
 }
 .custom-link:hover {
   cursor:pointer;
+}
+
+.v-card {
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
 }
 </style>
